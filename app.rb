832b99ec8d8e698f532453baeb66f5f77e9912f7
Bill Kirtley <bill@spiffy.com>
2012-05-08 22:32:52 -0400

    Configure for heroku database

--- app.rb
+++ app.rb
@@ -9,13 +9,21 @@ require 'logger'
 require 'slim'
 require 'will_paginate'
 require 'will_paginate/active_record'
+require 'uri'
 
 logger = Logger.new(STDOUT)
 
+db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/abagnale')
+
 ActiveRecord::Base.establish_connection(
-   :adapter => 'postgresql',
-   :database =>  'abagnale'
+  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  :host     => db.host,
+  :username => db.user,
+  :password => db.password,
+  :database => db.path[1..-1],
+  :encoding => 'utf8'
 )
+
 # ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 require './cc'
