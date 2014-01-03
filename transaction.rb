aa014aff475cc79dd20e8d5144dd3b7ac06cd991
Braulio Carreno <bcarreno@yahoo.com>
2014-01-03 11:56:37 -0500

    Delete transaction records when limit reached

--- transaction.rb
+++ transaction.rb
@@ -1,4 +1,9 @@
 require 'active_record'
 
 class Transaction < ActiveRecord::Base
+  after_create do |transaction|
+    if transaction.id % 10 == 0 && (record_count = Transaction.count) > 7000
+      Transaction.delete_all
+    end
+  end
 end
