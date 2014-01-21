0b2da78250768e1e37c6818d22acbb866c40d5ac
Braulio Carreno <bcarreno@yahoo.com>
2014-01-21 13:24:15 -0500

    There's an issue with Rails 4 and Postgres Adapter: 'could not obtain a database connection within 5.000 seconds (waited 5.000 seconds)' https://github.com/rails/rails/issues/12867

--- db/schema.rb
+++ db/schema.rb
@@ -8,15 +8,12 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended that you check this file into your version control system.
+# It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140102221250) do
+ActiveRecord::Schema.define(:version => 20140102221250) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "transactions", force: true do |t|
-    t.string   "fullccnum",   null: false
+  create_table "transactions", :force => true do |t|
+    t.string   "fullccnum",   :null => false
     t.string   "name"
     t.integer  "amount"
     t.string   "order"
