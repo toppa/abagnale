0fdc78962d6cbfca91f0cc77cf469001245a3d33
Decklin Foster <decklin@red-bean.com>
2014-10-28 12:29:12 -0400

    Use the port from DATABASE_URL

--- app.rb
+++ app.rb
@@ -18,6 +18,7 @@ db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/abagnale')
 ActiveRecord::Base.establish_connection(
   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
   :host     => db.host,
+  :port     => db.port,
   :username => db.user,
   :password => db.password,
   :database => db.path[1..-1],
